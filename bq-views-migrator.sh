e4231904b56607ba193eda0651df526951d64168
Balazs Zsido <balazs.zsido@gmail.com>
2020-03-25 11:05:42 +0100

    first draft no testing

--- bq-views-migrator.sh
+++ bq-views-migrator.sh
@@ -3,6 +3,7 @@
 ORIGP="crypto-song-153217"
 NEWP="al-bi-bq-test"
 MAX=10000
+TEMPDIR="$HOME/bq-temp"
 
 bq --project-id="$ORIGP" ls -n "$MAX" | tail -n +3 | sed -e 's/ *//g' | \
 while read DS; do
@@ -12,10 +13,15 @@ while read DS; do
     bq ls -n "$MAX" "$DS" | grep VIEW | sed -e 's/ \+/ /g' | cut -d' ' -f2 | \
     while read VIEW; do
 
-        bq show --format=prettyjson --project-id="$ORIGP" "$DS"."$VIEW" > "$VIEW.json"
-        cat "$VIEW.json" | jq '.view.query' | sed -z 's/\\n/ /g' | sed -e 's/\"//g' > 
+        DSDIR="$TEMPDIR/$DS"
+        QFILE="$DSDIR/$VIEW.qfile"
+        mkdir -p "$DSDIR"
 
-        bq mk --project_id="$NEWP" --use_legacy_sql=false --view $TEST "$i".dim_affiliates_test_v
+        bq show --format=prettyjson --project-id="$ORIGP" "$DS"."$VIEW" > "$DSDIR/$VIEW.json"
+        cat "$DSDIR/$VIEW.json" | jq '.view.query' \
+            | sed -z 's/\\n/ /g' | sed -e 's/\"//g' > "$QFILE"
 
+        bq mk --project_id="$NEWP" --use_legacy_sql=false \
+              --view_udf_resource "$QFILE" "$DS"."$VIEW"
+    done
 done
-
