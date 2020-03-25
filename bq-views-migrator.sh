1144a7a319e9993d0a801ddc2d0d43bd54624aea
Balazs Zsido <balazs.zsido@gmail.com>
2020-03-25 10:20:19 +0100

    working on loops, changing vars etc.

--- bq-views-migrator.sh
+++ bq-views-migrator.sh
@@ -2,14 +2,20 @@
 
 ORIGP="crypto-song-153217"
 NEWP="al-bi-bq-test"
+MAX=10000
 
-bq --project-id="$ORIGP" ls | tail -n +3 | sed -e 's/ *//g' | \
-while read i; do
-    echo "this is $i"
+bq --project-id="$ORIGP" ls -n "$MAX" | tail -n +3 | sed -e 's/ *//g' | \
+while read DS; do
+    
+    echo "this is $DS"
 
-    bq show --format=prettyjson --project-id="$ORIGP" [DATASET].[VIEW] > view.json
-    export TEST="$(cat dim_affiliates_v.json | jq '.view.query' | sed -z 's/\\n/ /g' | sed -e 's/\"//g')"
-    bq mk --project_id=al-bi-bq-test --use_legacy_sql=false --view $TEST dwh.dim_affiliates_test_v
+    bq ls -n "$MAX" "$DS" | grep VIEW | sed -e 's/ \+/ /g' | cut -d' ' -f2 | \
+    while read VIEW; do
+
+        bq show --format=prettyjson --project-id="$ORIGP" "$DS"."$VIEW" > "$VIEW.json"
+        cat "$VIEW.json" | jq '.view.query' | sed -z 's/\\n/ /g' | sed -e 's/\"//g' > 
+
+        bq mk --project_id="$NEWP" --use_legacy_sql=false --view $TEST "$i".dim_affiliates_test_v
 
 done
 
