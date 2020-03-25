a6663e81c5fa911aee3961f91993cc1ccc4d7529
Balazs Zsido <balazs.zsido@gmail.com>
2020-03-25 13:16:17 +0100

    removing leading 2 spaces

--- bq-views-migrator.sh
+++ bq-views-migrator.sh
@@ -3,19 +3,18 @@
 ORIGP="crypto-song-153217"
 NEWP="al-bi-bq-test"
 MAX=10000
-TEMPDIR="$HOME/bq-temp"
 
 bq --project_id="$ORIGP" ls -n "$MAX" | tail -n +3 | sed -e 's/ *//g' | \
 while read DS; do
     
-    echo "this is $DS"
+    echo "dataset is $DS"
 
     bq ls -n "$MAX" "$DS" | grep VIEW | sed -e 's/ \+/ /g' | cut -d' ' -f2 | \
     while read VIEW; do
 
         # hardcoded project name
         QUERY="$(bq show --format=sparse --view "$DS"."$VIEW" | tail -n +5 \
-                | sed -e 's/crypto-song-153217/al-bi-bq-test/g')"
+                | sed -e 's/^ \{2\}//g' | sed -e 's/crypto-song-153217/al-bi-bq-test/g')"
 
         # think about legacy sql param
         bq mk --project_id="$NEWP" --use_legacy_sql=false \
