1137c646b00b67b9b2f32a6e07f806de87479ddc
Balazs Zsido <balazs.zsido@gmail.com>
2020-06-03 18:43:56 +0200

    added copying tables

--- bq-views-migrator.sh
+++ bq-views-migrator.sh
@@ -7,9 +7,23 @@ MAX=10000
 bq --project_id="$ORIGP" ls -n "$MAX" | tail -n +3 | sed -e 's/ *//g' | \
 while read DS; do
    
+    # do it with while read
     echo "Creating $DS dataset"
-    bq --project_id=$NEWP mk $DS
+    bq --project_id="$NEWP" mk "$DS"
 
+    # do it with parallel first
+    echo "Copying tables for $DS"
+
+    bq ls -n "$MAX" "$DS" | grep TABLE | sed -e 's/ \+/ /g' | cut -d' ' -f2 | \
+    while read TABLE; do
+        
+        # -a append
+        # -n don't overwrite existing table
+        bq cp -n "$ORIGP":"$DS"."$TABLE" "$NEWP":"$DS"."$TABLE"
+
+    done
+
+    # do it with parallel after tables are finished
     echo "Copying views for $DS"
 
     bq ls -n "$MAX" "$DS" | grep VIEW | sed -e 's/ \+/ /g' | cut -d' ' -f2 | \
